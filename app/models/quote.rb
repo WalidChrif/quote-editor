8f7c54f5f059db752bc4747f5f3a07e88dbf17c7
walid chrif <walid.chrif111@gmail.com>
2025-01-06 15:20:21 +0100

    added subs to update and delete actions

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,10 @@
 class Quote < ApplicationRecord
-    validates :name, presence: true
+  include ActionView::RecordIdentifier
+  
+  validates :name, presence: true
 
-    after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
-  end
+  after_create_commit { broadcast_prepend_to "quotes" }
+  after_update_commit { broadcast_replace_to "quotes" }
+  # after_update_commit { broadcast_replace_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: dom_id(self) }
+  after_destroy_commit { broadcast_remove_to "quotes" }
+end
